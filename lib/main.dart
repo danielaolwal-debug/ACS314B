55428381c2546157a79030fd9d40ca4f524e5126
Daniela Olwal <danielaolwal@gmail.com>
2026-03-10 22:20:05 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,77 +19,122 @@ void main() {
         ),
         body: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Image.asset('assets/hendricks.webp'),
-              const Text(
-                "Login Screen",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Enter your username",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Use Email or Username",
-                  prefixIcon: Icon(Icons.person),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Enter your password",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              const TextField(
-                obscureText: true, // hides password input
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Enter your password",
-                  prefixIcon: Icon(Icons.lock), // corrected icon
-                ),
-              ),
-              const SizedBox(height: 20),
-              MaterialButton(
-                onPressed: () {},
-                color: Colors.grey,
-                child: const Text(
-                  "Login",
+          child: SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/hendricks.webp'),
+                const Text(
+                  "Login Screen",
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.black,
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Enter your username",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Use Email or Username",
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Enter your password",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                TextField(
+                  obscureText: true, // hides password input
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter your password",
+                    prefixIcon: Icon(Icons.lock), // corrected icon
+                    suffixIcon: Icon(
+                      Icons.visibility_off,
+                    ), // added visibility icon
+                  ),
+                ),
+                const SizedBox(height: 20),
+                MaterialButton(
+                  onPressed: () {},
+                  color: Colors.grey,
+                  textColor: Colors.teal,
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Row(
+                    children: [
+                      Text("dont have an account?"),
+                      SizedBox(width: 5),
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Reset Password",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
